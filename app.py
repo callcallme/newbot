ebb5d8000ab026e9f2ea58c4b28caf53df300fc6
jokeone <10026035@nhu.edu.tw>
2019-08-07 20:59:38 +0800

    Init

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ handler = WebhookHandler('86e586703b29cb67f92813076f135842')
 def keySearch(word):
     
     info ={'蘋果':'醫生遠離你','美女':'搜尋中','bike':'中文叫腳踏車','聯成電腦':'good'}
-    return info.get(word,'請輸入(樣板)(蘋果)(美女)(bike)(聯成電腦)(新北自行車)(新北自行車)(韓國瑜)')
+    return info.get(word,'請輸入(樣板)(蘋果)(美女)(bike)(聯成電腦)(新北自行車)(台中日月潭)(日月潭)(新北自行車)(韓國瑜)')
 content = ''
 
 
@@ -126,22 +126,12 @@ def handle_message(event):
                     MessageTemplateAction(
                         label='請按我!!',
                         text = '按到了~.~'
-                        ),
-                    MessageTemplateAction(
+                        ),MessageTemplateAction(
                         label='台北捷運!!',
                         text = '北捷'
-                        ),
-                    MessageTemplateAction(
+                        ),MessageTemplateAction(
                         label='高雄捷運',
                         text = '高捷'
-                        ),
-                    MessageTemplateAction(
-                        label='日月潭',
-                        text = '日月潭'
-                        ),
-                    MessageTemplateAction(
-                        label='台中日月潭'
-                        text = '台中日月潭'
                         )
                     ]
                 )
